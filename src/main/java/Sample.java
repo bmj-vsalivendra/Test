bc15ac06cceeb083b42c0e43b69eaa891ae7a2d7
riyan <riyan@riyan.home>
2016-04-01 20:35:01 +0100

    first project commit

---
+++ src/main/java/Sample.java
@@ -0,0 +1,40 @@
+
+
+
+
+
+  public class Sample {
+	 
+
+		//globel declaration 
+		int k =50;
+		
+		int i = 10;
+		
+		
+		
+		public void  test()
+		{
+			//local variable
+			int k=20;
+			int i = 10;
+			
+			int j = i+ k;
+		}
+		
+		public void test1()
+		{
+          int k=10;
+		  int l= i+ k;
+			
+		}
+		
+		public void test3()
+		{
+			int z = k;
+		}
+		
+	
+
+}
+
